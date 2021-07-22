1d6548960bd716877f3a0d7fb0d5e4cb3bff9f25
AlexanderCherednicheenko <acherednichenko174@gmail.com>
2021-07-22 15:16:07 +0300

    подключение слайдера games

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$('.games__items').slick({
+
+   slidesToShow: 1,
+   slidesToScroll: 4,
+   dots: false,
+   infinite: false, 
+   variableWidth: true,
+   centerMode: true,
+   focusOnSelect: true,
+  //  zIndex: 10,
+ });
